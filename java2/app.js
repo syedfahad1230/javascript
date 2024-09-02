d3b00e64fb3938cbc59ddb25f075f42a493e556a
syedfahad26 <168951538+syedfahad1230@users.noreply.github.com>
2024-09-02 12:16:57 +0500

    Add files via upload

---
+++ java2/app.js
@@ -0,0 +1,152 @@
+// var city ='karachi'
+// var greetUser="hello world"
+// alert("greetUser")
+
+// console.log(2+'2')
+// console.log('2'-'2')
+
+// var username;  // declare kra ha
+// var username = 'abdullah' // declare + assign
+// var username = "usman" //redeclare
+// username = "ammar" //reassign
+
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// %
+
+// var num1 = 10;
+// var num2 = 20; 
+
+// var sum = num1 + num2;
+// var subtract = num1 - num2;
+// var multiply = num1 * num2;
+// var division = num1 / num2;
+// var modulus = num2 % num1;
+// console.log(modulus)
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+// var num1 =20;
+// num1=num1+5;
+// num1 += 5;
+// num1 -= 5
+// num1 /= 5
+// num1 *= 5
+// console.log(num1)
+
+//Comparision Operator
+
+//comparision operator hamesha boolean return krta ha.
+
+// var num1=20;
+// var num2=20;
+// console.log(10<20)
+// console.log(10>20)
+// console.log(20<=20)
+// console.log(20>=20)
+// bara h ya barabar
+// console.log(20==20)
+// console.log(20=='20')
+// console.log(20==='20')//data type check krega
+// console.log(20===20)// ni krega
+// console.log(20!=20)
+// console.log(20!='20')
+// console.log(20!==20)
+// console.log(20!=='20')//type chck krga
+
+// var username= prompt("Enter your full name")
+// console.log(username)
+
+// var num1=Number(prompt('Enter first number'))
+// var num2=Number(prompt('Enter second number'))
+
+// console.log('sum of num1 and num2',num1+num2)
+
+// var username=20
+// console.log(typeof(true))
+
+// var num="20"
+// console.log(+num)
+
+// percentage calculator
+
+// var totalMarks=400
+// var engMarks = +prompt('enter your eng marks')
+// var urduMarks =+prompt('enter your urdu marks')
+// var islMarks = +prompt('enter your isl marks')
+// var phyMarks = +prompt('enter your phy marks')
+
+// var obtainedMarks = engMarks + urduMarks + islMarks + phyMarks
+
+// var percentage= obtainedMarks / totalMarks * 100
+
+// console.log('you got' + percentage + '%')
+
+
+// alert("greetUser")
+// alert('Error! Please enter a valid password.')
+// alert('Welcome to JS Land...\nHappy Coding!')
+// alert('Welcome to JS Land...')
+
+
+// alert('Happy Coding!')
+// var username
+// var myName= 'syed fahad'
+// console.log(myName)
+//      var message=''
+// var message='hello world'
+// alert(message)
+
+// var bio = 'jhone doe'
+// alert(bio)
+// var age = '15 years old'
+// alert(age)
+// var course= 'certified mobile'
+// alert(course)
+// var food= 'pizza\npizz\npiz\npi\np'
+// alert(food)
+// var email="syedfahadqamer@gmail.com"
+// alert(email)
+// document.write('Yah! I can write HTML content through js')
+// var string= '------@~^#------'
+// alert(string)
+// var visit = 14;
+// alert(`you have visited this site ${visit} times`);
+// var birthyear = 'my birth year is 2003'
+// document.write(birthyear)
+// var VisitorsName = 'jhon doe'
+// var producttitle = ' on xyz clothing store'
+// var quantity = ' ordered 5 T-Shirt(s)'
+// document.write(VisitorsName, quantity , producttitle)
+
+// legal variables
+// var myName
+// var my_name
+// var $name
+// var _name
+// var userNname
+
+// illegal variables
+// var 3num
+// var #time
+// var my name
+// var my-toString
+// var let
+
+// const num1=(prompt('first num'))
+// const num2=(prompt('second num'))
+// console.log('sum of num1 and num2 is',+num1+ +num2)
+// const sum=(`sum of num1 and num2 is ${num1 + num2}`)
+// const h1=document.querySelector('h1')
+// h1.innerHTML= sum
+
+
