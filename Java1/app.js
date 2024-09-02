ead38757a35c2fff43b155d84282629fc550c2d4
syedfahad26 <168951538+syedfahad1230@users.noreply.github.com>
2024-09-02 12:14:01 +0500

    Add files via upload

---
+++ Java1/app.js
@@ -0,0 +1,67 @@
+// console.log("Hello world");
+// console.log(10/5);
+// console.log("hello javasscript")
+// console.log(2+2)
+// console.log("2"+"2")
+
+// Data types
+
+//string "324skdla;skd#@$@#$"
+//number 4234.23423
+//boolean true | false
+
+// concatenation
+// console.log("2a"+"2b");
+// console.log("My name is fahad")
+// console.log(true)
+// console.log(20)
+// console.log("20")
+
+// alert("Hello world!");
+
+// alert(2+2);
+// console.log('abc');
+
+// variables
+
+// var city = "karachi";
+
+// var username = 'abdullah';
+// var age = 20;
+// var isLoggedIn = true;
+// console.log(username,age,isLoggedIn)
+
+
+// var num1 = "20";
+// var num2 = "30";
+// console.log(num1 + num2);
+
+// var 123username = 'abdullah'; xxxx
+// var %username123 = 'abdullah'; xxxx 
+// var $_username123 = 'abdullah';
+// // var full Name = 'abdullah';xxxx 
+// var fullName = 'abdullah';
+
+// console.log(fullName);
+
+// console.log(dsadsjajdsjkh) xxx
+// console.log($_username123);
+
+// var username;
+// console.log(username);
+
+//var city='karachi';// decleration
+// console.log(city);
+
+// var city = 'islamabad'; //redeclare
+// console.log(city);
+
+// var city = 'lahore'; //reassign
+//  console.log(city);
+
+// var weight = 29;
+// console.log(weight + 30)
+var fullname = 'abdullah'
+
+var rose = 'flower';
+var Rose = 'abc'
