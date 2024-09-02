74b400237cf279b159b8a0cad632580dd6378af5
syedfahad26 <168951538+syedfahad1230@users.noreply.github.com>
2024-09-02 12:53:50 +0500

    Add files via upload

---
+++ date & time/app.js
@@ -0,0 +1,27 @@
+// var rightNow = new Date('november 27, 2003');
+// console.log(rightNow.getTime())
+// console.log(rightNow.toString())
+// console.log(typeof rightNow)
+// console.log(rightNow.getDay())//jo day hga uska num aega
+// console.log(rightNow.getMonth())//jo month hoga usse aik pehla
+// console.log(rightNow.getDate())
+// console.log(rightNow.getFullYear());
+
+// console.log(rightNow.getHours());
+// console.log(rightNow.getMinutes());
+// console.log(rightNow.getSeconds());
+// console.log(rightNow.getMilliseconds());
+
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// console.log(dayNames[rightNow.getDay()])
+
+// roman na apni zindagi ka kitna waqt barbaad krdia
+
+
+// var currentDate = new Date();
+// var romanBirthDate = new Date("september 26, 2003");
+
+// var currentMilliseconds = currentDate.getTime()
+// var romanMilliseconds = romanBirthDate.getTime()
+// var remainingMilliseconds = currentMilliseconds - romanMilliseconds
+// console.log(Math.round(remainingMilliseconds/1000/60/60/24/365))
