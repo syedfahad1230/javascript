c2fea9e68aa72cbe7ff9078eb98ba07ea3956fa2
syedfahad26 <168951538+syedfahad1230@users.noreply.github.com>
2024-09-02 12:54:28 +0500

    Add files via upload

---
+++ switch case & hoisting/app.js
@@ -0,0 +1,109 @@
+
+// switch case
+// hoisting
+// let const
+// set interval
+// set timeout
+
+
+
+// let num = 11;
+
+// if (num > 10) {
+//     console.log('if chal rha ha');
+// } else {
+//     console.log('else chal rha ha');
+
+// }
+
+//  let num= 4
+
+// switch (num) {
+//     case 5:
+//         console.log('if chl rha he')
+//     case 4:
+//         console.log('4 chl rha he')
+//         break
+//         case 3:
+//             console.log('3 chl rha he')
+//     default:
+//         console.log('else chl rha he')
+// }
+
+// switch (true) {
+//         case num > 5:
+//             console.log("if chal rha ha");
+//             break
+//         default:
+//             console.log('else chal rha ha');
+
+//     }
+
+
+
+// hoisting
+
+// console.log(username);
+// var username = "abdullah";//ni chlega
+
+// hello()
+// chl rha h
+// function hello(){
+//     console.log('hello world!')
+// }
+
+
+
+// var  x
+//hoisting yes
+//redeclare yes
+//reassign yes
+
+// let
+//hoisting no
+//redeclare no
+//reassign yes
+
+
+
+// const
+//hoisting no
+//redeclare no
+//reassign no
+
+
+
+
+// var city = "karachi"; 
+// var city = "lahore";
+// city = "islamabad";
+// console.log(city)
+
+// let username = 'abullah'
+// // let username = 'usman'
+// username = 'usman'
+// console.log(username)
+
+//  const username = 'usman'
+// console.log(username) 
+
+
+
+
+//settimeout(function , millisecond)
+
+// setTimeout(function (){
+//     console.log("console after one second")
+// } , 5000)
+
+
+// setInterval(function (){
+//     console.log("console after one second")
+// } , 1000)
+
+// let num =0
+// setInterval(function(){
+//     num+=1
+//     console.log(num)
+// },1000)
+
