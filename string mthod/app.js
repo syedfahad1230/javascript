d723f463d73fb87adcda99e1b89238ad458413ce
syedfahad26 <168951538+syedfahad1230@users.noreply.github.com>
2024-09-02 12:50:22 +0500

    Add files via upload

---
+++ string mthod/app.js
@@ -0,0 +1,109 @@
+// console.log('hello world!');
+
+
+// var userInput = prompt("enter city name");
+// if(userInput.toLowerCase() === "karachi"){
+//     console.log("you live in the cleanest city");
+// }else if(userInput.toLowerCase() === 'lahore'){
+//     console.log('you live in the safest city');
+
+// }else{
+//     console.log("koi faida nahi zindagi ka");
+
+// }
+
+
+
+// tolowercase
+// touppercase
+
+
+// var username = 'abDulLAh';
+// console.log(username.toUpperCase());
+// console.log(username.toLowerCase());
+
+
+
+//length
+//charAt
+//slice
+//indexof
+//replace
+//replaceAll
+//trim
+//split
+
+
+// var username =' abDulLAh';
+// console.log(username.length);
+// console.log(username[2]);
+// console.log(username.charAt(3))
+// var copystr= username.slice(2,6)//6 wala chor dega
+// console.log(copystr) 
+// console.log(username.indexOf('h'))
+
+
+// var str = "you live in the safest city karachi. it is the best city in the world karachi.";
+// console.log(str.split(" "))//gap nai dege to sb tota aega 
+// for (var i = 0; i < str.length; i++){
+//   console.log(str[i])
+// }
+// var cleanestCities = ['karachi', 'lahore', 'peshawar', 'quetta'];
+// var copystr= cleanestCities.slice( 0,2)
+// console.log(copystr)
+// console.log(str.replace('karachi','lahore'))
+// console.log(str.replaceAll('karachi','lahore'))
+
+
+
+// let cleanestCity = ['karachi', 'lahore', 'peshawar', 'quetta']
+// console.log(cleanestCity.indexOf('karachi'))
+// console.log(cleanestCity.includes('lahore'))//true / false dega
+
+// var input = prompt('enter city').toLowerCase()
+
+// if( cleanestCity.includes(input)){//mtlb input agr mojod hoa to sai h
+//     console.log('you live in safest city')
+// }else{
+//     console.log('war gya')
+// }
+
+// for (var i = 0; i < cleanestCity.length; i++){
+//     if (cleanestCity[i] === input) {//mtlb input agr mojod hoa to sai h
+//         console.log('you live in safest city')
+//     } 
+// }
+// var username = "      abdullah ";
+// var removeSpaces = username.trim();
+// console.log(removeSpaces.length);
+
+
+// reverse string
+
+// var city = "karachi";
+
+// console.log(city.split('').reverse().join("")  )
+// var input=document.querySelector('#userinput')
+// var para =document.querySelector('#result')
+
+// function chkpalindrime() {
+//     var palindrome = input.value.split('').reverse().join('')
+//     if (palindrome === input.value) {
+//         console.log('it is palindrome')
+//         para.innerHTML='it is palindrome'
+//     } else {
+//         console.log('it is not palindrome')
+//      para.innerHTML='it is not palindrome'
+//     }
+// }
+
+
+// let username = 'abullah'
+// let username = 'usman'
+// username = 'usman'
+// console.log(username)
+
+// const username = 'abdullah'
+
+// username = 'usman'
+// console.log(username)
