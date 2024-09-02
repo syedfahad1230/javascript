16c2eb50560ffba8eff84897e548f511bb63e592
syedfahad26 <168951538+syedfahad1230@users.noreply.github.com>
2024-09-02 12:22:21 +0500

    Add files via upload

---
+++ dom manipulation/app.js
@@ -0,0 +1,82 @@
+
+// pre increment
+// post increment
+
+// num++ post increment
+// ++num pre increment
+
+// var num = 10;
+// console.log(num++); // 10 uska baad 11
+// console.log(++num); // 11 + 1 = 12 
+
+
+// var cal = num++ + num++ + ++num + num++ + ++num;
+// console.log(10 + 11 + 13 + 13 + 15)
+// console.log(cal)
+
+
+// var fullname = document.querySelector('#fullname');
+
+// function getValues(){
+//     console.log(fullname.value)
+//     fullname.value = ""
+// }
+// data deke submit krana console me 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num= document.querySelector('#num')
+// var num2=document.querySelector('#num2')
+// var sum=document.querySelector('#sum')
+
+
+// function getvalue(){
+//     // console.log(num.value)
+//     // console.log(num2.value)
+
+//     // console.log(+num.value + +num2.value)
+//     var sumOfnum = +num.value + +num2.value
+//     sum.innerHTML = 'the sum of num and num2 is ' + sumOfnum
+// }
+// getvalue()
+
+
+// var totalmarks=400
+// var eng=document.querySelector('#eng')
+// var urdu=document.querySelector('#urdu')
+// var math=document.querySelector('#math')
+// var isl=document.querySelector('#isl')
+
+// function getvalue(){
+//    var obtainedMarks = +eng.value + +urdu.value + +math.value + +isl.value
+//    console.log('marks obtained ' +  obtainedMarks)
+// var percentage = obtainedMarks / totalmarks * 100
+// console.log('you got' + percentage + '%')
+
+// if (percentage >= 80) {
+//     console.log('A+ grade');
+// } else if (percentage >= 70) {
+//     console.log('A grade');
+// } else if (percentage >= 60) {
+//     console.log('B grade')
+// } else {
+//     console.log('fail');
+// }
+// }
+// getvalue()
+
